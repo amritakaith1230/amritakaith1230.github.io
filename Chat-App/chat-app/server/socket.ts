f0ae519a6f78821b3eb32d1a1644af47dd05024d
Amrita Kaithwas <amritakaithwas4@gmail.com>
2025-10-13 09:54:36 +0530

    Update socket.ts

--- Chat-App/chat-app/server/socket.ts
+++ Chat-App/chat-app/server/socket.ts
@@ -57,12 +57,9 @@ export function initializeSocket(httpServer: HTTPServer) {
   const io = new SocketIOServer(httpServer, {
     cors: {
       origin:
-       process.env.NODE_ENV === "production"
-          ? [process.env.FRONTEND_URL, "https://amritakaith1230-github-io-ne34.vercel.app"]
-          : [
-              "http://localhost:3000",
-              "https://amritakaith1230-github-io-2.onrender.com",
-            ],
+        process.env.NODE_ENV === "production"
+          ? process.env.FRONTEND_URL
+          : ["http://localhost:3000", "http://localhost:3001"],
       methods: ["GET", "POST"],
       credentials: true,
     },
